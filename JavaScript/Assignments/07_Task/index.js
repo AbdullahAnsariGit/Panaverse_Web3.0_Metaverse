bc5427e9d42226234b25af3c222b26c0f8dc69dc
AbdullahAnsariGit <abdullahbusiness11@gmail.com>
2022-09-28 15:02:03 +0500

    7 Task

---
+++ JavaScript/Assignments/07_Task/index.js
@@ -0,0 +1,30 @@
+// Q Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
+// 4+4 = 8
+// 12-4 = 8
+// 2 * 4 = 8
+// 64 / 8 = 8
+
+// Addition
+document.write(`<h1>Addition</h1>`)
+var a = 4;
+var b = 4;
+var result = a+b;
+document.write(`${a} + ${b} = ${result}`)
+// Subtraction
+document.write(`<h1>Subtraction</h1>`)
+var a = 12;
+var b = 4;
+var result = a-b;
+document.write(`<br>${a} - ${b} = ${result}`)
+// Multipication
+document.write(`<h1>Multipication</h1>`)
+var a = 2;
+var b = 4;
+var result = a*b;
+document.write(`<br>${a} x ${b} = ${result}`)
+// Division
+document.write(`<h1>Division</h1>`)
+var a = 64;
+var b = 8;
+var result = a/b;
+document.write(`<br>${a} / ${b} = ${result}`)
