d03b521a8e7239902e9c1d9dee26bd79f4adfdb1
AbdullahAnsariGit <abdullahbusiness11@gmail.com>
2022-10-01 04:20:27 +0500

    push

---
+++ JavaScript/Assignments/14-20_Task/index.js
@@ -0,0 +1,14 @@
+//------------------------Task 14------------------------//
+// Q Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d like to invite to dinner. Then use your list to print a message to each person, inviting them to dinner.
+
+var guest = ["fawad","faaiz","asad"]
+var message = "you are invited to dinner at 8pm"
+
+document.write(`${guest[0]} ${message}, ${guest[1]} ${message}, ${guest[2]} ${message}`)
+
+//------------------------Task 15------------------------//
+//------------------------Task 16------------------------//
+//------------------------Task 17------------------------//
+//------------------------Task 18------------------------//
+//------------------------Task 19------------------------//
+//------------------------Task 20------------------------//
